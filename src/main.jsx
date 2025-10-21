b19cd7be4a3d2534004b9f13957eff851cc1956e
Jennifer Hansson <jennifermhansson@gmail.com>
2025-10-21 12:49:09 +0200

    new name input field

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-/* import App from './App.jsx' */
-import Timer from './timer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx"; // ✅ vi importerar App istället för Timer direkt
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Timer />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
