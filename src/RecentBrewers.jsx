4d32b54cfea8cafe31479f14e450e16172fe1a66
Jennifer Hansson <jennifermhansson@gmail.com>
2025-10-28 14:11:38 +0100

    Add css (#3)
    * added database
    
    * added database, kudos, timestamp
    
    * added basic css

--- src/RecentBrewers.jsx
+++ src/RecentBrewers.jsx
@@ -5,12 +5,15 @@ const RecentBrewers = ({ brewers, giveKudos }) => {
       <ul>
         {brewers.map((brewer) => (
           <li key={brewer.id}>
-            <strong>{brewer.name}</strong>
-            <br />
-            <small>{new Date(brewer.time).toLocaleString()}</small>
-            <br />
-            <span>Kudos: {brewer.kudos}</span>
-            <button onClick={() => giveKudos(brewer.id)}>Give Kudos</button>
+            <div className="name-time-container">
+              <strong>{brewer.name}</strong>
+
+              <small>{new Date(brewer.time).toLocaleString()}</small>
+            </div>
+            <div className="kudos-container">
+              <span>Kudos: {brewer.kudos}</span>
+              <button onClick={() => giveKudos(brewer.id)}>Give Kudos</button>
+            </div>
           </li>
         ))}
       </ul>
