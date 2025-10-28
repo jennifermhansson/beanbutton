b14ec2f1cf1169728fe3c1b2c6fd95f84b6ef97c
Jennifer Hansson <jennifermhansson@gmail.com>
2025-10-28 13:37:04 +0100

    added database, kudos, timestamp

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getDatabase } from "firebase/database";
 
 // TODO: Replace with your own Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCR7SJSoBaGhgktw7bBQ9qqalPztI-J108",
+  authDomain: "coffeebutton-249dd.firebaseapp.com",
+  databaseURL:
+    "https://coffeebutton-249dd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "coffeebutton-249dd",
+  storageBucket: "coffeebutton-249dd.firebasestorage.app",
+  messagingSenderId: "277126949131",
+  appId: "1:277126949131:web:17aebfe2c6fd74934e5a0e",
 };
 
 // Initialize Firebase
